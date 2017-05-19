b6a7e3410c411b4bd46780875c7822bfb4c0b274
fwmeng88 <fwmeng88@gmail.com>
2017-05-19 00:05:10 -0400

    add a usful configuration instruction

--- vim_configure.sh
+++ vim_configure.sh
@@ -76,3 +76,5 @@ let g:syntastic_always_populate_loc_list = 1
 let g:syntastic_auto_loc_list = 1
 let g:syntastic_check_on_open = 1
 let g:syntastic_check_on_wq = 0
+
+#This also might interesting to you https://realpython.com/blog/python/vim-and-python-a-match-made-in-heaven/
